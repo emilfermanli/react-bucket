e7dd6ca1c00081c16df8a0d5ceef3e5c7f349fb9
emil fermanli <emilfermanli@gmail.com>
2020-04-08 14:05:22 +0400

    Updates

---
+++ precache-manifest.8f671094c735adc202ab773660f74c7d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "476c478fef31f0ab6f7b9c7269caff9f",
+    "url": "/react-bucket/index.html"
+  },
+  {
+    "revision": "ea3c2854be977c6fbb73",
+    "url": "/react-bucket/static/css/2.765f46be.chunk.css"
+  },
+  {
+    "revision": "8cd6fa447cf79e2106ea",
+    "url": "/react-bucket/static/css/main.9d43707a.chunk.css"
+  },
+  {
+    "revision": "ea3c2854be977c6fbb73",
+    "url": "/react-bucket/static/js/2.df4ab62b.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/react-bucket/static/js/2.df4ab62b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cd6fa447cf79e2106ea",
+    "url": "/react-bucket/static/js/main.b6cbeb27.chunk.js"
+  },
+  {
+    "revision": "12efbece2756555e4ff0",
+    "url": "/react-bucket/static/js/runtime-main.46dc36a9.js"
+  },
+  {
+    "revision": "06c336a90032a00f01d6cd4f653cb3fa",
+    "url": "/react-bucket/static/media/bg_1.06c336a9.jpg"
+  },
+  {
+    "revision": "497b6d3ab8815f47032945b8c72c6467",
+    "url": "/react-bucket/static/media/bg_2.497b6d3a.jpg"
+  },
+  {
+    "revision": "a9138984550c8afae44203538972f04b",
+    "url": "/react-bucket/static/media/bg_5.a9138984.jpg"
+  },
+  {
+    "revision": "ca1606aba339bf6e410fa049bf5e436b",
+    "url": "/react-bucket/static/media/bg_6.ca1606ab.jpg"
+  }
+]);
